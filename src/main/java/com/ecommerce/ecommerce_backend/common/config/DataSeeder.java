abdebb8191780350ca279bf1f76f3d33b0f78ece
Nour Eldin <118866955+NourAldeen001@users.noreply.github.com>
2026-06-14 18:15:54 +0300

    config: add DataSeeder to seed ROLE_USER and ROLE_ADMIN on startup

---
+++ src/main/java/com/ecommerce/ecommerce_backend/common/config/DataSeeder.java
@@ -0,0 +1,29 @@
+package com.ecommerce.ecommerce_backend.common.config;
+
+import com.ecommerce.ecommerce_backend.role.Role;
+import com.ecommerce.ecommerce_backend.role.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataSeeder implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if(roleRepository.findByName("ROLE_USER").isEmpty()) {
+            Role userRole = new Role();
+            userRole.setName("ROLE_USER");
+            roleRepository.save(userRole);
+        }
+
+        if(roleRepository.findByName("ROLE_ADMIN").isEmpty()) {
+            Role adminRole = new Role();
+            adminRole.setName("ROLE_ADMIN");
+            roleRepository.save(adminRole);
+        }
+    }
+}
