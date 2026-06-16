641bc65521d00debe92f00da541669f370637d11
Nour Eldin <118866955+NourAldeen001@users.noreply.github.com>
2026-06-16 22:20:33 +0300

    config: update SecurityConfig with JWT filter and route protection

--- src/main/java/com/ecommerce/ecommerce_backend/common/config/SecurityConfig.java
+++ src/main/java/com/ecommerce/ecommerce_backend/common/config/SecurityConfig.java
@@ -1,28 +1,79 @@
 package com.ecommerce.ecommerce_backend.common.config;
 
+import com.ecommerce.ecommerce_backend.common.security.JwtAuthenticationFilter;
+import com.ecommerce.ecommerce_backend.common.security.JwtService;
+import com.ecommerce.ecommerce_backend.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        JwtAuthenticationFilter jwtAuthFilter = new JwtAuthenticationFilter(
+                jwtService,
+                userDetailsService()
+        );
 
         http
             .csrf(csrfConfigurer -> csrfConfigurer.disable()) // disable for REST APIs
             .authorizeHttpRequests(auth -> auth
-                    .anyRequest().permitAll()
-            );
+                    .requestMatchers("/api/v1/auth/**").permitAll()
+                    .requestMatchers("/api/v1/users/register").permitAll()
+                    .anyRequest().authenticated()
+            )
+            .sessionManagement(session -> session
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .authenticationProvider(authenticationProvider())
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    // Tells Spring how to load users and verify password
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username ->  userRepository.findByEmail(username)
+                        .orElseThrow(() -> new UsernameNotFoundException(
+                                "User not found with email: " + username
+                        ));
+    }
+
+    // Needed for AuthService to authenticate login requests
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
