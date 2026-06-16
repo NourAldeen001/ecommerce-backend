9376ddefc14c887d17b2adfda6afa78e266081d7
Nour Eldin <118866955+NourAldeen001@users.noreply.github.com>
2026-06-16 21:49:10 +0300

    feat(auth): add LoginRequest and AuthResponse DTOs

---
+++ src/main/java/com/ecommerce/ecommerce_backend/auth/dto/LoginRequest.java
@@ -0,0 +1,18 @@
+package com.ecommerce.ecommerce_backend.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LoginRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
