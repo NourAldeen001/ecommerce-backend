a22440d6ed1df2b60b4d06600254f74303b8a188
Nour Eldin <118866955+NourAldeen001@users.noreply.github.com>
2026-07-06 16:11:34 +0300

    config: permit Swagger UI URLs in SecurityConfig and SpringDoc configuration to application.yaml

--- src/main/java/com/ecommerce/ecommerce_backend/common/config/SecurityConfig.java
+++ src/main/java/com/ecommerce/ecommerce_backend/common/config/SecurityConfig.java
@@ -43,6 +43,12 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                     .requestMatchers("/api/v1/auth/**").permitAll()
                     .requestMatchers("/api/v1/users/register").permitAll()
+                    .requestMatchers(
+                            "/swagger-ui/**",
+                            "/swagger-ui.html",
+                            "/v3/api-docs/**",
+                            "/v3/api-docs.yaml"
+                    ).permitAll()
                     .anyRequest().authenticated()
             )
             .sessionManagement(session -> session
